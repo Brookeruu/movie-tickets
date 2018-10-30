abd9c844cf6a92bf5be56e182e1cf97ce19c1f32
Brooke Perry and Sheila Stephen <brooke@>
2018-10-30 14:43:09 -0700

    remove excess prototypes and change html options ie age to drop down

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,33 @@
-function makeTicket(){
-  this.index= [];
-  }
 
-  makeTicket.prototype.totalTicket = function(index){
-    var total = 0;
-    for(var i=0; i < index.length; i++){
-    total += index[i];
-    }
-    return total;
-  }
 
 function Ticket(movieName, showTime, age){
   this.movieName = movieName,
   this.showTime = showTime,
   this.age = age
+  this.index = [];
 }
 
-Ticket.prototype.ageValidation = function(age){
-  if(this.age) >= 65{
-    this.index[0].push(0);
-  }
-  this.index[0].push(2);
-}
-
-Ticket.prototype.getShowTime = function(showTime) {
-  if (this.showTime  === 1 || this.showTime === 2) {
-    this.index[1].push(6);
-  }
-  this.index[1].push(10);
-}
-
-Ticket.prototype.releaseValidation = function(movieName){
-  if(this.movieName === 1 || this.movieName === 2){
-    this.index[2].push(0);
-  }
-  this.index[2].push(2);
+Ticket.prototype.determineCost = function() {
+  this.cost = this.age + this.showTime + this.movieName;
+  return this.cost;
 }
 
 $(document).ready(function(){
-  $("#formid").submit(event){
+  $("#formId").submit(function(event){
     event.preventDefault();
-    var inputtedMovieName =
+    var inputtedMovieName = parseInt($("#movieName").val());
+    var inputtedMovieTime = parseInt($("#movieTime").val());
+    var inputtedAge = parseInt($("#age").val());
+
+    $("#movieName").val("");
+    $("#movieTime").val("");
+    $("#age").val("");
+
+
+    var newTicket = new Ticket(inputtedMovieName, inputtedMovieTime, inputtedAge);
+    console.log(newTicket);
+    var ticketCost = newTicket.determineCost();
+    console.log(ticketCost);
+
   });
 });
